42cb6158113d86f230c2199b3893c771cd96543c
Simon Harris <haruki_zaemon@mac.com>
2019-07-23 21:10:48 +1000

    Immutable!

--- kotlin/src/main/kotlin/CardDeck.kt
+++ kotlin/src/main/kotlin/CardDeck.kt
@@ -1,11 +1,12 @@
-class CardDeck(vararg cards: Card) : Iterable<Card> {
-    private val _cards: MutableList<Card> = cards.toMutableList()
-
+class CardDeck private constructor(private val cards: List<Card>) : Iterable<Card> {
     companion object {
         fun build(): CardDeck {
             val cards = mutableListOf<Card>()
             val suites = setOf(Suite.CLUBS, Suite.DIAMONDS, Suite.HEARTS, Suite.SPADES)
-            val ranks = setOf(Rank.ACE, Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX, Rank.SEVEN, Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING)
+            val ranks = setOf(
+                Rank.ACE, Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX, Rank.SEVEN,
+                Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING
+            )
 
             for (suite in suites) {
                 for (rank in ranks) {
@@ -16,46 +17,42 @@ class CardDeck(vararg cards: Card) : Iterable<Card> {
             cards.add(Card(Suite.WILD, Rank.JOKER))
             cards.add(Card(Suite.WILD, Rank.JOKER))
 
-            return CardDeck(*cards.toTypedArray())
+            return CardDeck(cards)
         }
     }
 
-    override fun equals(other: Any?): Boolean {
-        return when(other) {
-            is CardDeck -> {
-                val otherCards = other.fold(mutableListOf<Card>()) { acc, card ->
-                    acc.add(card)
-                    acc
-                }
-                return _cards == otherCards
-            }
-            else -> super.equals(other)
-        }
+    override fun equals(other: Any?): Boolean = when {
+        other is CardDeck -> cards == other.cards
+        else -> super.equals(other)
     }
 
-    override fun iterator() = _cards.iterator()
-    override fun hashCode() = _cards.hashCode()
+    override fun iterator() = cards.iterator()
+    override fun hashCode() = cards.hashCode()
 
-    fun size() = _cards.size
+    fun size() = cards.size
 
     fun shuffle(): CardDeck {
-        _cards.shuffle()
-        return this
+        return CardDeck(
+            cards.toMutableList()
+                .apply { shuffle() }
+        )
     }
 
-    fun draw() = _cards.removeAt(0)
+    fun draw(): Pair<Card, CardDeck> {
+        val remainingCards = cards.toMutableList()
+        val drawnCard = remainingCards.removeAt(0)
+        return drawnCard to CardDeck(remainingCards)
+    }
 
     fun cut(numberOfCards: Int) = Pair(
-            CardDeck(*_cards.take(numberOfCards).toTypedArray()),
-            CardDeck(*_cards.drop(numberOfCards).toTypedArray())
-        )
+        CardDeck(cards.take(numberOfCards)),
+        CardDeck(cards.drop(numberOfCards))
+    )
 
     operator fun plus(otherDeck: CardDeck): CardDeck {
-        for (card in otherDeck) {
-            _cards.add(card)
-        }
-
-        return this
+        return CardDeck(
+            cards.toMutableList()
+                .apply { addAll(otherDeck) }
+        )
     }
 }
-
